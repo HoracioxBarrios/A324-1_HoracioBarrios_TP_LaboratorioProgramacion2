c473c9b455fc2920b470044e9e8ecd8aabec69fc
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-05-25 00:21:23 -0300

    renombre el test

--- Entidades/EncargadoTest/EncargadoTest.cs
+++ Entidades/EncargadoTest/EncargadoTest.cs
@@ -3,10 +3,10 @@ using Entidades.Enumerables;
 namespace EncargadoTest
 {
     [TestClass]
-    public class TestEntidadEncargado
+    public class EncargadoTest
     {
         [TestMethod]
-        public void TestVerificarInstanciaDeEncargadoPruebaSiElSalarioObtenidoEsIgualDaTrue()
+        public void VerificarInstanciaDeEncargado_PruebaSiElSalarioObtenido_EsIgualDaTrue()
         {
             //Arrange
             string nombre = "Pibe";
@@ -15,10 +15,11 @@ namespace EncargadoTest
             ERol rol = ERol.Encargado;
             string direccion = "Av. San Pocho";
             decimal salario = 150000.50M;
-
-
+            //act
             Encargado encargado = new Encargado(nombre, apellido,contacto,direccion,salario);
             decimal salarioObtenido = encargado.Salario;
+
+            //assert
             Assert.AreEqual(salarioObtenido, salario);
 
         }
