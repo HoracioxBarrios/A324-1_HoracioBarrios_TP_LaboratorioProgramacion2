39962f488fb554a09c452e8632393f5b19a2e72e
HoracioxBarrios <horacioxbarrios@gmail.com>
2024-06-27 11:13:45 -0300

    Correcion de Test Ingrediente

--- Entidades/EncargadoTest/IngredienteTest.cs
+++ Entidades/EncargadoTest/IngredienteTest.cs
@@ -384,96 +384,100 @@ namespace Test
             }
         }
 
-        //[TestMethod]
-        //public void ElIngredienteQueEstaEnLitrosSeDescuentaEnMiliLitrosYActualizaElValorEnBaseASuCantidad_DebeDescontarElAceiteDeUnaListaDeProductosQueHayEnStock_ALCorroborarDebeQuedar9coma5LitrosYTambienDebeValerTodoNueveMilQuinientosPesos()
-        //{
+        [TestMethod]
+        public void ElIngredienteQueEstaEnLitrosSeDescuentaEnMiliLitrosYActualizaElValorEnBaseASuCantidad_DebeDescontarElAceiteDeUnaListaDeProductosQueHayEnStock_ALCorroborarDebeQuedar9coma5LitrosYTambienDebeValerTodoNueveMilQuinientosPesos()
+        {
 
-        //    //Arrange
-        //    //Ingrediente 1
-        //    ETipoDeProducto tipoDeProducto1 = ETipoDeProducto.Ingrediente;
-        //    string nombreDeProducto1 = "Aceite";
-        //    double cantidad = 10;
-        //    EUnidadDeMedida unidadDeMedida = EUnidadDeMedida.Litro;
-        //    decimal precio = 10000;
+            //Arrange
+            //Ingrediente 1
+            ETipoDeProducto tipoDeProducto1 = ETipoDeProducto.Ingrediente;
+            string nombreDeProducto1 = "Aceite";
+            double cantidad = 10;
+            EUnidadDeMedida unidadDeMedida = EUnidadDeMedida.Litro;
+            decimal precio = 10000;
 
-        //    var mockProveedor1 = new Mock<IProveedor>();
-        //    mockProveedor1.Setup(p => p.Nombre).Returns("Proveedor 1");
-        //    mockProveedor1.Setup(p => p.Cuit).Returns("30-12345678-9");
-        //    mockProveedor1.Setup(p => p.Direccion).Returns("Calle Falsa 123");
-        //    mockProveedor1.Setup(p => p.TipoDeProducto).Returns(ETipoDeProducto.Almacen);
-        //    mockProveedor1.Setup(p => p.MediosDePago).Returns(EMediosDePago.Transferencia);
-        //    mockProveedor1.Setup(p => p.EsAcreedor).Returns(EAcreedor.Si);
-        //    mockProveedor1.Setup(p => p.DiaDeEntrega).Returns(EDiaDeLaSemana.Lunes);
-        //    mockProveedor1.Setup(p => p.ID).Returns(1);
-        //    mockProveedor1.Setup(p => p.ToString()).Returns("ID: 1, Nombre: Proveedor 1, CUIT: 30-12345678-9, Direccion: Calle Falsa 123, Tipo de Producto que Provee: Almacen, Medio de Pago: Transferencia, Es Acreedor? : Si, Dia de Entrega: Lunes");
+            var mockProveedor1 = new Mock<IProveedor>();
+            mockProveedor1.Setup(p => p.Nombre).Returns("Proveedor 1");
+            mockProveedor1.Setup(p => p.Cuit).Returns("30-12345678-9");
+            mockProveedor1.Setup(p => p.Direccion).Returns("Calle Falsa 123");
+            mockProveedor1.Setup(p => p.TipoDeProducto).Returns(ETipoDeProducto.Almacen);
+            mockProveedor1.Setup(p => p.MediosDePago).Returns(EMediosDePago.Transferencia);
+            mockProveedor1.Setup(p => p.EsAcreedor).Returns(EAcreedor.Si);
+            mockProveedor1.Setup(p => p.DiaDeEntrega).Returns(EDiaDeLaSemana.Lunes);
+            mockProveedor1.Setup(p => p.ID).Returns(1);
+            mockProveedor1.Setup(p => p.ToString()).Returns("ID: 1, Nombre: Proveedor 1, CUIT: 30-12345678-9, Direccion: Calle Falsa 123, Tipo de Producto que Provee: Almacen, Medio de Pago: Transferencia, Es Acreedor? : Si, Dia de Entrega: Lunes");
 
-        //    //Ingrediente 2 
-        //    ETipoDeProducto tipoDeProducto2 = ETipoDeProducto.Ingrediente;
-        //    string nombreDeProducto2 = "Carne";
-        //    double cantidad2 = 50;
-        //    EUnidadDeMedida unidadDeMedida2 = EUnidadDeMedida.Kilo;
-        //    decimal precio2 = 20000;
+            //Ingrediente 2 
+            ETipoDeProducto tipoDeProducto2 = ETipoDeProducto.Ingrediente;
+            string nombreDeProducto2 = "Carne";
+            double cantidad2 = 50;
+            EUnidadDeMedida unidadDeMedida2 = EUnidadDeMedida.Kilo;
+            decimal precio2 = 20000;
 
-        //    var mockProveedor2 = new Mock<IProveedor>();
-        //    mockProveedor2.Setup(p => p.Nombre).Returns("Proveedor 2");
-        //    mockProveedor2.Setup(p => p.Cuit).Returns("31-12345678-8");
-        //    mockProveedor2.Setup(p => p.Direccion).Returns("Calle Falsa 456");
-        //    mockProveedor2.Setup(p => p.TipoDeProducto).Returns(ETipoDeProducto.Carniceria);
-        //    mockProveedor2.Setup(p => p.MediosDePago).Returns(EMediosDePago.Tarjeta);
-        //    mockProveedor2.Setup(p => p.EsAcreedor).Returns(EAcreedor.No);
-        //    mockProveedor2.Setup(p => p.DiaDeEntrega).Returns(EDiaDeLaSemana.Martes);
-        //    mockProveedor2.Setup(p => p.ID).Returns(2);
-        //    mockProveedor2.Setup(p => p.ToString()).Returns("ID: 2, Nombre: Proveedor 2, CUIT: 31-12345678-8, Direccion: Calle Falsa 456, Tipo de Producto que Provee: Carniceria, Medio de Pago: Tarjeta, Es Acreedor? : No, Dia de Entrega: Martes");
+            var mockProveedor2 = new Mock<IProveedor>();
+            mockProveedor2.Setup(p => p.Nombre).Returns("Proveedor 2");
+            mockProveedor2.Setup(p => p.Cuit).Returns("31-12345678-8");
+            mockProveedor2.Setup(p => p.Direccion).Returns("Calle Falsa 456");
+            mockProveedor2.Setup(p => p.TipoDeProducto).Returns(ETipoDeProducto.Carniceria);
+            mockProveedor2.Setup(p => p.MediosDePago).Returns(EMediosDePago.Tarjeta);
+            mockProveedor2.Setup(p => p.EsAcreedor).Returns(EAcreedor.No);
+            mockProveedor2.Setup(p => p.DiaDeEntrega).Returns(EDiaDeLaSemana.Martes);
+            mockProveedor2.Setup(p => p.ID).Returns(2);
+            mockProveedor2.Setup(p => p.ToString()).Returns("ID: 2, Nombre: Proveedor 2, CUIT: 31-12345678-8, Direccion: Calle Falsa 456, Tipo de Producto que Provee: Carniceria, Medio de Pago: Tarjeta, Es Acreedor? : No, Dia de Entrega: Martes");
 
-        //    //Ingrediente que se instancia en el plato y estaria en la lista del plato.
-        //    //Ingrediente 3
-        //    ETipoDeProducto tipoDeProducto3 = ETipoDeProducto.Ingrediente;
-        //    string nombreDeProducto3 = "Aceite";
-        //    double cantidad3 = 500; // Cantidad que usa en el plato
-        //    EUnidadDeMedida unidadDeMedida3 = EUnidadDeMedida.MiliLitro;
-        //    decimal precio3 = 1000;
+            //Ingrediente que se instancia en el plato y estaria en la lista del plato.
+            //Ingrediente 3
+            ETipoDeProducto tipoDeProducto3 = ETipoDeProducto.Ingrediente;
+            string nombreDeProducto3 = "Aceite";
+            double cantidad3 = 500; // Cantidad que usa en el plato
+            EUnidadDeMedida unidadDeMedida3 = EUnidadDeMedida.MiliLitro;
+            decimal precio3 = 1000;
 
-        //    var mockProveedor3 = new Mock<IProveedor>();
-        //    mockProveedor3.Setup(p => p.Nombre).Returns("Proveedor 1");
-        //    mockProveedor3.Setup(p => p.Cuit).Returns("30-12345678-9");
-        //    mockProveedor3.Setup(p => p.Direccion).Returns("Calle Falsa 123");
-        //    mockProveedor3.Setup(p => p.TipoDeProducto).Returns(ETipoDeProducto.Almacen);
-        //    mockProveedor3.Setup(p => p.MediosDePago).Returns(EMediosDePago.Transferencia);
-        //    mockProveedor3.Setup(p => p.EsAcreedor).Returns(EAcreedor.Si);
-        //    mockProveedor3.Setup(p => p.DiaDeEntrega).Returns(EDiaDeLaSemana.Lunes);
-        //    mockProveedor3.Setup(p => p.ID).Returns(1);
-        //    mockProveedor3.Setup(p => p.ToString()).Returns("ID: 1, Nombre: Proveedor 1, CUIT: 30-12345678-9, Direccion: Calle Falsa 123, Tipo de Producto que Provee: Almacen, Medio de Pago: Transferencia, Es Acreedor? : Si, Dia de Entrega: Lunes");
+            var mockProveedor3 = new Mock<IProveedor>();
+            mockProveedor3.Setup(p => p.Nombre).Returns("Proveedor 1");
+            mockProveedor3.Setup(p => p.Cuit).Returns("30-12345678-9");
+            mockProveedor3.Setup(p => p.Direccion).Returns("Calle Falsa 123");
+            mockProveedor3.Setup(p => p.TipoDeProducto).Returns(ETipoDeProducto.Almacen);
+            mockProveedor3.Setup(p => p.MediosDePago).Returns(EMediosDePago.Transferencia);
+            mockProveedor3.Setup(p => p.EsAcreedor).Returns(EAcreedor.Si);
+            mockProveedor3.Setup(p => p.DiaDeEntrega).Returns(EDiaDeLaSemana.Lunes);
+            mockProveedor3.Setup(p => p.ID).Returns(1);
+            mockProveedor3.Setup(p => p.ToString()).Returns("ID: 1, Nombre: Proveedor 1, CUIT: 30-12345678-9, Direccion: Calle Falsa 123, Tipo de Producto que Provee: Almacen, Medio de Pago: Transferencia, Es Acreedor? : Si, Dia de Entrega: Lunes");
 
-        //    GestorDeProductos gestorDeProductos = new GestorDeProductos();
-
-
-        //    //Act
-        //    //Productos que van a estar en la lista del stock (está dentro de GestorProductos)
-        //    gestorDeProductos.CrearProductoParaListaDeStock(tipoDeProducto1, nombreDeProducto1, cantidad, unidadDeMedida, precio, mockProveedor1.Object);
-        //    gestorDeProductos.CrearProductoParaListaDeStock(tipoDeProducto2, nombreDeProducto2, cantidad2, unidadDeMedida2, precio2, mockProveedor2.Object);
+            GestorDeProductos gestorDeProductos = new GestorDeProductos();
 
 
-        //    //Producto que va a estar en el plato(lo que  usa el plato)
-        //    IProducto ingrediente3 = gestorDeProductos.CrearProducto(tipoDeProducto3, nombreDeProducto3, cantidad3, unidadDeMedida3, precio3, mockProveedor3.Object);
+            //Act
+            //Productos que van a estar en la lista del stock
+            IProducto aceite = gestorDeProductos.CrearProducto(tipoDeProducto1, nombreDeProducto1, cantidad, unidadDeMedida, precio, mockProveedor1.Object);
+            IProducto carne = gestorDeProductos.CrearProducto(tipoDeProducto2, nombreDeProducto2, cantidad2, unidadDeMedida2, precio2, mockProveedor2.Object);
 
-        //    List<IConsumible> listaDeIngredienteEnElPlato = new List<IConsumible>();
-        //    listaDeIngredienteEnElPlato.Add((IConsumible)ingrediente3);
-
-        //    //Se le pasa la lista de Ingredintes a descontar
-        //    bool seDesconto = gestorDeProductos.DescontarProductosDeStock(listaDeIngredienteEnElPlato);
-
-
+            //AGREGAMOS A STOCK
+            gestorDeProductos.AgregarProductoAStock(aceite);
+            gestorDeProductos.AgregarProductoAStock(carne);
 
 
+            //Producto que va a estar en el plato(lo que  usa el plato)
+            IProducto aceite2 = gestorDeProductos.CrearProducto(tipoDeProducto3, nombreDeProducto3, cantidad3, unidadDeMedida3, precio3, mockProveedor3.Object);
 
-        //    foreach (IProducto productoIngrediente in gestorDeProductos.ReadAllProductos())
-        //    {
-        //        if (productoIngrediente.Nombre == "Aceite" && productoIngrediente.Cantidad == 8.5)
-        //        {
-        //            Assert.AreEqual(8.5, productoIngrediente.Cantidad);
-        //            Assert.AreEqual(9500, productoIngrediente.CalcularPrecioDeCosto());
-        //        }
-        //    }
-        //}
+            List<IConsumible> listaDeIngredienteEnElPlato = new List<IConsumible>();
+            listaDeIngredienteEnElPlato.Add((IConsumible)aceite2);
+
+            //Se le pasa la lista de Ingredintes a descontar
+            bool seDesconto = gestorDeProductos.DescontarProductosDeStock(listaDeIngredienteEnElPlato);
+
+
+
+
+
+            foreach (IProducto productoIngrediente in gestorDeProductos.ReadAllProductos())
+            {
+                if (productoIngrediente.Nombre == "Aceite" && productoIngrediente.Cantidad == 8.5)
+                {
+                    Assert.AreEqual(8.5, productoIngrediente.Cantidad);
+                    Assert.AreEqual(9500, productoIngrediente.CalcularPrecioDeCosto());
+                }
+            }
+        }
     }
 }
